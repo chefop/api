90b8e4eea99bde63cf1b2607233593f94f60cd09
PREVOST Nathan <nathanp33@laposte.net>
2019-02-08 17:31:22 +0100

    fix: typo

--- api/models/MainCourse.js
+++ api/models/MainCourse.js
@@ -2,66 +2,52 @@ const mongoose = require("mongoose");
 
 MainCourseSchema = new mongoose.Schema({
 
-    name: {
-        type: String,
-        required: [true, 'Name is required.'],
-        unique: true
-    },
+  name: {
+      type: String,
+      required: [true, 'Name is required.'],
+      unique: true
+  },
 
-    description: {
-        type: String,
-        default: ''
-    },
+  description: {
+      type: String,
+      default: ''
+  },
 
-    df_price: {
-        type: Number,
-        required: [true, 'Price is required.']
-    },
+  df_price: {
+      type: Number,
+      required: [true, 'Price is required.']
+  },
 
-    vat: {
-        type: Number,
-        required: [true, 'Taxes are required.']
-    },
+  vat: {
+      type: Number,
+      required: [true, 'Taxes are required.']
+  },
 
-    quantity: {
-        type: Number,
-        default: 0
-    },
+  quantity: {
+      type: Number,
+      default: 0
+  },
 
-    allergen: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Allergen"
-    },
+  allergen: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Allergen"
+  },
 
-    photo: {
-        type: String,
-        default: ''
-    },
+  photo: {
+      type: String,
+      default: ''
+  },
 
-    baking: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Baking"
-    },
-
-    created_at: {
-        type: Date,
-        default: Date.now
-    },
-
-    updated_at: {
-        type: Date,
-        default: Date.now
-    }
+  baking: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Baking"
+  }
 
 }, {
-    shardKey: {
-        _id: "hashed"
-    }
-});
-
-MainCourseSchema.pre("save", function(next) {
-    this.updated_at = Date.now()
-    next();
+  timestamp: true,
+  shardKey: {
+      _id: "hashed"
+  }
 });
 
 MainCourseSchema.methods.toJSON = function() {
