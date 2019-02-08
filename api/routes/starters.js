ef30d044f9c9f043be878a042a23ff5c07509c18
PREVOST Nathan <nathanp33@laposte.net>
2019-02-08 14:25:46 +0100

    refactor: condition starter

--- api/routes/starters.js
+++ api/routes/starters.js
@@ -25,113 +25,107 @@ router.get('/', (req, res, next) => {
                 message: 'Starters fetched successfully',
                 starters: starters
             });
-        }).catch(err => { // If ko status 500 sans message
-            res.status(500).json({ message: err.message });
         });
 });
 
 // Post one starter
 router.post('/', (req, res, next) => {
-    if (utils.requestIsEmpty(req.body)) { // Check if request body is empty
+    if (utils.requestIsEmpty(req.body))  // Check if request body is empty
         res.status(400).json({ message: 'Cannot create starter, empty request.' }); // If enmpty status 400 and send message
-    }
-    // Create a starter with body request
-    const starter = new Starter({
-        name: req.body.name,
-        description: req.body.description,
-        df_price: req.body.df_price,
-        vat: req.body.vat,
-        quantity: req.body.quantity,
-        allergen: req.body.allergen,
-        photo: req.body.photo,
-    });
+    else{
+      // Create a starter with body request
+      const starter = new Starter({
+          name: req.body.name,
+          description: req.body.description,
+          df_price: req.body.df_price,
+          vat: req.body.vat,
+          quantity: req.body.quantity,
+          allergen: req.body.allergen,
+          photo: req.body.photo,
+      });
 
-    starter
-        .save() // Save starter
-        .then(result => {
-            res.status(200).json({ // If ok status 200, send message and datas
-                message: 'New starter created with success.',
-                starter: starter
-            });
-        }).catch(err => { // If ko status 500 and send message
-            res.status(500).json({ message: err.message });
-        });
+      starter
+          .save() // Save starter
+          .then(result => {
+              res.status(200).json({ // If ok status 200, send message and datas
+                  message: 'New starter created with success.',
+                  starter: starter
+              });
+          }).catch(err => { // If ko status 500 and send message
+              res.status(500).json({ message: err.message });
+          });
+    }
 });
 
 // Delete a starter
 router.delete('/:id', (req, res, next) => {
-    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) { // Check if id is empty, status 400 and message
+    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) // Check if id is empty, status 400 and message
         res.status(400).json({ message: 'Cannot delete starter, empty request.' });
+    else {
+      const starter = Starter
+          .findOne({ // Find one stater by id
+              _id: req.params.id
+          })
+          .then(starter => {
+              Starter.deleteOne({ // Delete one starter
+                  _id: req.params.id
+              }).then(result => {
+                  res.status(200).json({ // If ok status 200 and send message
+                      message: 'Starter deleted',
+                      starter: starter
+                  });
+              });
+          });
     }
-    const starter = Starter
-        .findOne({ // Find one stater by id
-            _id: req.params.id
-        })
-        .then(starter => {
-            Starter.deleteOne({ // Delete one starter
-                _id: req.params.id
-            }).then(result => {
-                res.status(200).json({ // If ok status 200 and send message
-                    message: 'Starter deleted',
-                    starter: starter
-                });
-            }).catch(err => { // If ko status 500 and send message
-                res.status(500).json({ message: err.message });
-            });
-        }).catch(err => { // If ko status 500 and send message
-            res.status(500).json({ message: err.message });
-        });
 });
 
 // Get one starter
 router.get('/:id', (req, res, next) => {
-    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) { // Check if id is empty, status 400 and message
+    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id))  // Check if id is empty, status 400 and message
         res.status(400).json({ message: 'Cannot get starter, empty request.' });
+    else{
+      const starter = Starter
+          .findOne({ // Find one stater by id
+              _id: req.params.id
+          })
+          .then(starter => {
+              res.status(200).json({ // If ok status 200, send message and datas
+                  message: 'Starter fetched successfully',
+                  starter: starter
+              });
+          });
     }
-    const starter = Starter
-        .findOne({ // Find one stater by id
-            _id: req.params.id
-        })
-        .then(starter => {
-            res.status(200).json({ // If ok status 200, send message and datas
-                message: 'Starter fetched successfully',
-                starter: starter
-            });
-        }).catch(err => { // If ko status 500 and send message
-            res.status(500).json({ message: err.message });
-        });
 });
 
 // Update one starter
 router.put('/:id', (req, res, next) => {
-    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id)) { // Check if id is empty, status 400 and message
+    if (utils.requestIsEmpty(req.params.id) || !ObjectId.isValid(req.params.id))  // Check if id is empty, status 400 and message
         res.status(400).json({ message: 'Cannot put starter, empty request.' });
-    }
-    const starter = Starter
-        .findOne({ // Find one stater by id
-            _id: req.params.id
-        })
-        .then(starter => { // Save data
-            starter.name = req.body.name;
-            starter.description = req.body.description;
-            starter.df_price = req.body.df_price;
-            starter.vat = req.body.vat;
-            starter.quantity = req.body.quantity;
-            starter.allergen = req.body.allergen;
-            starter.photo = req.body.photo;
+    else{
+      const starter = Starter
+          .findOne({ // Find one stater by id
+              _id: req.params.id
+          })
+          .then(starter => { // Save data
+              starter.name = req.body.name;
+              starter.description = req.body.description;
+              starter.df_price = req.body.df_price;
+              starter.vat = req.body.vat;
+              starter.quantity = req.body.quantity;
+              starter.allergen = req.body.allergen;
+              starter.photo = req.body.photo;
 
-            starter.save()
-            .then(starter => {
-                res.status(200).json({ // If ok status 200, send message and datas
-                    message: 'Starter updated with success.',
-                    starter: starter
-                });
-            }).catch(err => { // If ko status 500 and send message
-                res.status(500).json({ message: err.message });
-            });
-        }).catch(err => { // If ko status 500 and send message
-            res.status(500).json({ message: err.message });
-        });
+              starter.save()
+              .then(starter => {
+                  res.status(200).json({ // If ok status 200, send message and datas
+                      message: 'Starter updated with success.',
+                      starter: starter
+                  });
+              }).catch(err => { // If ko status 500 and send message
+                  res.status(500).json({ message: err.message });
+              });
+          });
+    }
 });
 
 module.exports = router;
